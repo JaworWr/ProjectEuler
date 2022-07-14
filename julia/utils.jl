f657cbca19410ac64f6d5451e1b5593a3f22ab0e
Michał Jaworski <jawor403@gmail.com>
2022-07-14 23:38:27 +0200

    Modular arithmetic utils

--- julia/utils.jl
+++ julia/utils.jl
@@ -64,3 +64,63 @@ end
 export is_prime
 
 end
+
+module ModularArithmetic
+
+legendre(n::Integer, p::Integer) = powermod(n, (p-1) ÷ 2, p)
+export legendre
+
+
+function tonelli_shanks(n::Integer, p::Integer)
+    if n == 0
+        return 0
+    end
+    if legendre(n, p) != 1
+        throw(DomainError((n, p), "square root does not exist"))
+    end
+
+    q = p - 1
+    s = 0
+    while q % 2 == 0
+        q ÷= 2
+        s += 1
+    end
+
+    z = 0
+    for i in 2:p-1
+        if legendre(i, p) != 1
+            z = i
+            break
+        end
+    end
+
+    m = s
+    c = powermod(z, q, p)
+    t = powermod(n, q, p)
+    r = powermod(n, (q+1) ÷ 2, p)
+    while true
+        if t == 0
+            return 0
+        elseif t == 1
+            r = min(r, p - r)
+            return r
+        end
+        i = 0
+        tmp = t
+        while tmp != 1
+            tmp = powermod(tmp, 2, p)
+            i += 1
+        end
+        b = c
+        for _ in 1:m-i-1
+            b = powermod(b, 2, p)
+        end
+        m = i
+        c = powermod(b, 2, p)
+        t = t * c % p
+        r = r * b % p
+    end
+end
+export tonelli_shanks
+
+end
